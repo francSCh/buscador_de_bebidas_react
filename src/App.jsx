d90ae3a6008d1a726cf32869ab32ed95da762573
francSCh <salaschfr@gmail.com>
2022-11-11 10:02:21 -0600

    Nos conectamos a la API y obtenemos las categorías

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { Container } from 'react-bootstrap';
+import Formulario from './components/Formulario';
+import { CategoriasProvider } from './context/CategoriasProvider';
+
 function App() {
 
   return (
-    <h1>Bebidas</h1>
+    <CategoriasProvider>
+      <header className="py-5">
+        <h1>Buscador de Bebidas</h1>
+      </header>
+
+      <Container className='mt-5'>
+        <Formulario />
+      </Container>
+    </CategoriasProvider>
   )
 }
 
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,47 @@
+import { Button, Form, Row, Col } from 'react-bootstrap';
+import useCategorias from '../hooks/useCategorias';
+
+const Formulario = () => {
+    const { categorias } = useCategorias();
+
+    return (
+        <div>
+            <Form>
+                <Row>
+                    <Col md={6}>
+                        <Form.Group className='mb-3'>
+                            <Form.Label htmlFor='nombre'>Nombre Bebida</Form.Label>
+
+                            <Form.Control id='nombre' type='text' placeholder='Ej: Tequila, Vodka, etc.' name='nombre' />
+                        </Form.Group>
+
+                    </Col>
+                    <Col md={6}>
+                        <Form.Group className='mb-3'>
+                            <Form.Label htmlFor='categoria'>Categoría Bebida</Form.Label>
+
+                            <Form.Select id='categoria' name='categoria'>
+                                <option>- Selecciona Categoría -</option>
+                                {categorias.map(categoria => (
+                                    <option key={categoria.strCategory} value={categoria.strCategory}>
+                                        {categoria.strCategory}
+                                    </option>
+                                ))}
+                            </Form.Select>
+                        </Form.Group>
+                    </Col>
+                </Row>
+
+                <Row className='justify-content-end'>
+                    <Col md={3}>
+                        <Button variant='danger' className='text-uppercase w-100'>
+                            Buscar Bebidas
+                        </Button>
+                    </Col>
+                </Row>
+            </Form>
+        </div>
+    )
+}
+
+export default Formulario
